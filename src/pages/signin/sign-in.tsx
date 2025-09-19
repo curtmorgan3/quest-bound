aee73b0193e632857a07f96a15e7623d354bd688
curtmorgan3 <curtmorgan3@gmail.com>
2025-09-19 13:58:33 -0600

    feat: Signin animation

--- src/pages/signin/sign-in.tsx
+++ src/pages/signin/sign-in.tsx
@@ -1,4 +1,5 @@
-import { AnimatedSplashCard, Button, Input, Link } from '@/components';
+import videoSrc from '@/assets/logo-animation.mp4';
+import { Button, Input, Link } from '@/components';
 import {
   Select,
   SelectContent,
@@ -43,55 +44,56 @@ export const SignIn = () => {
 
   return (
     <div className='flex flex-col align-center gap-8'>
-      <AnimatedSplashCard delay={0.5} />
+      <video autoPlay muted src={videoSrc} className='max-h-[400px]'>
+        Your browser does not support the video tag.
+      </video>
 
+      <div className='flex flex-col gap-6 items-center justify-center'>
+        <div className='flex gap-4 items-center'>
+          <Select onValueChange={(value) => setSelectedUserId(value)} value={selectedUserId}>
+            <SelectTrigger className='w-[200px]'>
+              <SelectValue placeholder='Select a user' />
+            </SelectTrigger>
+            <SelectContent>
+              {users?.map((user) => (
+                <SelectItem className='w-[200px]' key={user.id} value={user.id}>
+                  {user.username}
+                </SelectItem>
+              ))}
+              <SelectItem className='w-[200px]' value='_new'>
+                New User
+              </SelectItem>
+            </SelectContent>
+          </Select>
+          {selectedUserId === '_new' && (
+            <Input
+              className='w-[200px]'
+              placeholder='Username'
+              value={newUsername}
+              onChange={(e) => setNewUsername(e.target.value)}
+            />
+          )}
+          <Button
+            loading={loading}
+            onClick={handleLogin}
+            disabled={!selectedUserId || (selectedUserId === '_new' && !newUsername)}>
+            Submit
+          </Button>
+        </div>
+      </div>
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1, delay: 2.5 }}>
-        <div className='flex flex-col gap-6 items-center justify-center'>
-          <div className='flex gap-4 items-center'>
-            <Select onValueChange={(value) => setSelectedUserId(value)} value={selectedUserId}>
-              <SelectTrigger className='w-[200px]'>
-                <SelectValue placeholder='Select a user' />
-              </SelectTrigger>
-              <SelectContent>
-                {users?.map((user) => (
-                  <SelectItem className='w-[200px]' key={user.id} value={user.id}>
-                    {user.username}
-                  </SelectItem>
-                ))}
-                <SelectItem className='w-[200px]' value='_new'>
-                  New User
-                </SelectItem>
-              </SelectContent>
-            </Select>
-            {selectedUserId === '_new' && (
-              <Input
-                className='w-[200px]'
-                placeholder='Username'
-                value={newUsername}
-                onChange={(e) => setNewUsername(e.target.value)}
-              />
-            )}
-            <Button
-              loading={loading}
-              onClick={handleLogin}
-              disabled={!selectedUserId || (selectedUserId === '_new' && !newUsername)}>
-              Submit
+        <div className='flex flex-col gap-4 items-center'>
+          <Link href='https://docs.questbound.com' target='_blank'>
+            Learn More
+          </Link>
+          <a target='_blank' href='https://discord.gg/7QGV4muT39' data-testid='join-discord'>
+            <Button variant='ghost' className='cursor-pointer'>
+              <img alt='Discord' src={DiscordImage} style={{ height: 30, width: 30 }} />
             </Button>
-          </div>
-
-          <div className='flex flex-col gap-4 items-center'>
-            <Link href='https://docs.questbound.com' target='_blank'>
-              Learn More
-            </Link>
-            <a target='_blank' href='https://discord.gg/7QGV4muT39' data-testid='join-discord'>
-              <Button variant='ghost'>
-                <img alt='Discord' src={DiscordImage} style={{ height: 30, width: 30 }} />
-              </Button>
-            </a>
-          </div>
+          </a>
         </div>
       </motion.div>
     </div>
