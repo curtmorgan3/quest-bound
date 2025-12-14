4c4b989b1a891807fc60226dd3f12198110fa39a
curtmorgan3 <curtmorgan3@gmail.com>
2025-12-14 15:21:11 -0600

    fix: grid type updates

--- client/src/libs/compass-core-composites/composites/grid/grid.tsx
+++ client/src/libs/compass-core-composites/composites/grid/grid.tsx
@@ -1,10 +1,16 @@
-import { Loader } from '@/libs/compass-core-ui';
-import { ColDef, GridApi as _GridApi } from 'ag-grid-community';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-quartz.css';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  AllCommunityModule,
+  ModuleRegistry,
+  type ColDef,
+  type GridApi as _GridApi,
+} from 'ag-grid-community';
 import { AgGridReact } from 'ag-grid-react';
 import './grid.css';
 
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface GridColumn<T> extends ColDef {}
 export type CellRendererProps<T> = { data: T };
 export type GridApi = _GridApi;
@@ -13,7 +19,7 @@ interface Props<T> {
   rowData: T[];
   colDefs: GridColumn<T>[];
   onSelectionChanged?: (selections: T[]) => void;
-  onCellValueChanged?: (data: T, columnId: string) => void;
+  onCellValueChanged?: (data: T, columnId: string, rowIndex: number | null) => void;
   onDragEnd?: (api: GridApi) => void;
   onDragStopped?: (api: GridApi) => void;
   onDragLeave?: (api: GridApi) => void;
@@ -33,9 +39,8 @@ export const Grid = <T,>({
   onColumnMoved,
 }: Props<T>) => {
   return (
-    <div className='ag-theme-quartz-dark' style={{ height: '100%', width: '100%' }}>
+    <div className='ag-theme-quartz-dark w-full h-full'>
       <AgGridReact
-        loadingOverlayComponent={<Loader color='info' />}
         rowData={rowData}
         columnDefs={colDefs}
         rowDragManaged
@@ -54,10 +59,8 @@ export const Grid = <T,>({
         onRowDragEnter={(e) => {
           onDragEnter?.(e.api);
         }}
-        suppressRowClickSelection
-        reactiveCustomComponents
         onSelectionChanged={(e) => onSelectionChanged?.(e.api.getSelectedRows())}
-        onCellValueChanged={(e) => onCellValueChanged?.(e.data, e.column.getColId())}
+        onCellValueChanged={(e) => onCellValueChanged?.(e.data, e.column.getColId(), e.rowIndex)}
       />
     </div>
   );
