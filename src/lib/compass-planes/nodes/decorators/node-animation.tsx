b1a87f92e2c473774d9386c183246ccb9889e6b6
curtmorgan3 <curtmorgan3@gmail.com>
2026-02-24 21:48:12 -0600

    feat: feature flag component animations

--- src/lib/compass-planes/nodes/decorators/node-animation.tsx
+++ src/lib/compass-planes/nodes/decorators/node-animation.tsx
@@ -1,3 +1,4 @@
+import { useFeatureFlag } from '@/hooks/use-feature-flag';
 import { CharacterContext } from '@/stores';
 import type { Component } from '@/types';
 import { useContext, type ReactNode } from 'react';
@@ -9,16 +10,19 @@ interface NodeAnimationProps {
 }
 
 export const NodeAnimation = ({ component, children }: NodeAnimationProps) => {
+  const animationEnabled = useFeatureFlag('animation', false);
   const context = useContext(CharacterContext);
   const character = context?.character;
-
   const { value, attributeType } = useNodeData(component);
-
   const { flashKey, diff } = useRegisterAnimation(
     character?.id,
     component.attributeId ?? '',
     attributeType === 'number' ? parseInt(`${value}`) : value,
   );
+
+  if (!animationEnabled) {
+    return <>{children}</>;
+  }
 
   return (
     <div key={flashKey}>
