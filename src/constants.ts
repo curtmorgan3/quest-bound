7dd0f969540b45806cd3b9bb69f16f0c26b22bac
curtmorgan3 <curtmorgan3@gmail.com>
2026-02-26 17:45:13 -0600

    chore: discord link

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const DOCS_URL = 'https://docs.questbound.com';
-export const DISCORD_URL = 'https://discord.gg/Zx2jR5Q3zN';
+export const DISCORD_URL = 'https://discord.gg/7QGV4muT39';
 export const DEFAULT_TILE_RENDER_SIZE = 32;
 export const MAX_LOCATION_MAP_ASSET_WIDTH = 2560;
 export const MAX_LOCATION_MAP_ASSET_HEIGHT = 1920;
